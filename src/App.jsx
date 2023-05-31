f332ceb7b3ceb578ec82fddb53a7864cbb31e176
DenVis <itdeniro00@gmail.com>
2023-05-31 09:39:09 +0300

    makу code lighter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Square from "./components/squares";
 
 function App() {
 	const [squares, setSquares] = useState(Array(9).fill(null));
 	const [isXNext, setIsXNext] = useState(true);
 	const [winner, setWinner] = useState(null);
-	
 
 	const handleClick = (i) => {
 		if (squares[i] || winner) return;
+
 		const squaresCopy = [...squares];
 		squaresCopy[i] = isXNext ? "X" : "O";
+
 		setSquares(squaresCopy);
 		setIsXNext(!isXNext);
+
 		const lines = [
 			[0, 1, 2],
 			[3, 4, 5],
@@ -22,6 +25,7 @@ function App() {
 			[0, 4, 8],
 			[2, 4, 6],
 		];
+
 		lines.forEach((line) => {
 			if (squaresCopy[line[0]] !== null) {
 				if (
@@ -29,87 +33,61 @@ function App() {
 					squaresCopy[line[1]] === squaresCopy[line[2]]
 				) {
 					squaresCopy[line[0]] === "X"
-						? setWinner("Winner X")
-						: setWinner("Winner 0");
+						? setWinner("Winner is X")
+						: setWinner("Winner is 0");
 				}
-				if (winner) {
-					resetGame()
-				}else{
-					return 
-				}
+
+			
 			}
-
 		});
-	}
-		
-	
-	const resetGame = () => {
-			setSquares(Array(9).fill(null));
-			setIsXNext(true);
-			setWinner(null);
 	};
-		
-	
 
-	useEffect(() => {
-		if (winner) {
-			document.getElementById("btn").style.display = "flex";
-		} else {
-			document.getElementById("btn").style.display = "none";
-		}
-	});
+	const resetGame = () => {
+		setSquares(Array(9).fill(null));
+		setIsXNext(true);
+		setWinner(null);
+	};
+
+	
 
 	return (
 		<>
+			{winner && (
+				<div className="winner-box">
+					
+					<p className="winner">{winner}</p>
+				</div>
+			)}
 			<div className="title-app">
 				<h1>Tic Tac Toe</h1>
-				<p>who is the winner</p>
-				<p className="winner">{winner}</p>
 			</div>
 			<div className="board">
 				<div className="board-row">
-					<button className="square" onClick={() => handleClick(0)}>
-						{squares[0]}
-					</button>
-					<button className="square" onClick={() => handleClick(1)}>
-						{squares[1]}
-					</button>
-					<button className="square" onClick={() => handleClick(2)}>
-						{squares[2]}
-					</button>
+					<Square value={squares[0]} onClick={() => handleClick(0)} />
+					<Square value={squares[1]} onClick={() => handleClick(1)} />
+					<Square value={squares[2]} onClick={() => handleClick(2)} />
 				</div>
 				<div className="board-row">
-					<button className="square" onClick={() => handleClick(3)}>
-						{squares[3]}
-					</button>
-					<button className="square" onClick={() => handleClick(4)}>
-						{squares[4]}
-					</button>
-					<button className="square" onClick={() => handleClick(5)}>
-						{squares[5]}
-					</button>
+					<Square value={squares[3]} onClick={() => handleClick(3)} />
+					<Square value={squares[4]} onClick={() => handleClick(4)} />
+					<Square value={squares[5]} onClick={() => handleClick(5)} />
 				</div>
 				<div className="board-row">
-					<button className="square" onClick={() => handleClick(6)}>
-						{squares[6]}
-					</button>
-					<button className="square" onClick={() => handleClick(7)}>
-						{squares[7]}
-					</button>
-					<button className="square" onClick={() => handleClick(8)}>
-						{squares[8]}
-					</button>
+					<Square value={squares[6]} onClick={() => handleClick(6)} />
+					<Square value={squares[7]} onClick={() => handleClick(7)} />
+					<Square value={squares[8]} onClick={() => handleClick(8)} />
 				</div>
-				<div className="new-game" id="btn">
-					{winner && ( 
-					<button className="newgame" onClick={() => resetGame() }>
-						New Game
-					</button>
-					)}
-				</div>
+
+				{winner && (
+					<div className="new-game" id="btn">
+						<button className="newgame" onClick={() => resetGame()}>
+							New Game
+						</button>
+					</div>
+				)}
 			</div>
 		</>
-	)
+	);
 }
 
 export default App;
